ca3e861940e9390c2d531ae954d16bf470a091ca
Thibault Martin <twenty2bar@gmail.com>
2023-12-02 00:49:32 +0100

    [day 1] better solution

--- day1/main.ml
+++ day1/main.ml
@@ -17,11 +17,15 @@ end
 module Solving = struct
   open Base
 
-  type t = Int of int | Str of int [@@deriving show]
+  type t = Int of int | Str of int
 
   let none_f ~f v = match v with None -> f () | Some _ -> v
 
-  let convert s =
+  let get = function Int i | Str i -> i
+
+  exception Found of t option
+
+  let search ~f s pos c =
     let size = String.length s in
     let check part pos res =
       let len = String.length part in
@@ -30,36 +34,42 @@ module Solving = struct
         let s' = String.sub s ~pos:(pos + 1) ~len in
         if String.equal s' part then Some (Str res) else None
     in
-    String.foldi s ~init:[] ~f:(fun i acc c ->
-        let res =
-          match c with
-          | c when Char.is_digit c -> Some (Int (Char.get_digit_exn c))
-          | 'o' -> check "ne" i 1
-          | 't' -> none_f ~f:(fun () -> check "hree" i 3) (check "wo" i 2)
-          | 'f' -> none_f ~f:(fun () -> check "ive" i 5) (check "our" i 4)
-          | 's' -> none_f ~f:(fun () -> check "even" i 7) (check "ix" i 6)
-          | 'e' -> check "ight" i 8
-          | 'n' -> check "ine" i 9
-          | _ -> None
-        in
-        match res with None -> acc | Some v -> v :: acc)
-    |> List.rev
+    if f c then
+      match c with
+      | c when Char.is_digit c -> Some (Int (Char.get_digit_exn c))
+      | 'o' -> check "ne" pos 1
+      | 't' -> none_f ~f:(fun () -> check "hree" pos 3) (check "wo" pos 2)
+      | 'f' -> none_f ~f:(fun () -> check "ive" pos 5) (check "our" pos 4)
+      | 's' -> none_f ~f:(fun () -> check "even" pos 7) (check "ix" pos 6)
+      | 'e' -> check "ight" pos 8
+      | 'n' -> check "ine" pos 9
+      | _ -> None
+    else None
 
-  let is_int = function Int _ -> true | Str _ -> false
-  let get = function Int i | Str i -> i
+  let find_first ~f s = String.find_mapi s ~f:(search ~f s)
+
+  let find_last ~f s =
+    let len = String.length s in
+    try
+      for i = 0 to len - 1 do
+        let id = (len - 1) - i in
+        match search ~f s id s.[id] with
+        | None -> ()
+        | Some v -> raise (Found (Some v))
+      done;
+      assert false
+    with Found v -> v
 
   let part_aux ~f input =
-    let f l =
-      let x = f l in
-      let y = List.rev l |> f in
-      match (x, y) with
-      | Some i1, Some i2 -> (get i1 * 10) + get i2
+    let f s =
+      match find_first ~f s, find_last ~f s with
+      | Some i1, Some i2 -> ((get i1) * 10) + (get i2)
       | _ -> 0
     in
-    List.map ~f:convert input |> List.sum ~f (module Int)
+    List.sum ~f (module Int) input
 
-  let part1 (input : input) : output = part_aux ~f:(List.find ~f:is_int) input
-  let part2 (input : input) : output = part_aux ~f:List.hd input
+  let part1 (input : input) : output = part_aux ~f:Char.is_digit input
+  let part2 (input : input) : output = part_aux ~f:(fun _ -> true) input
 end
 
 module Today = MakeDay (Types) (Parsing) (Solving)
