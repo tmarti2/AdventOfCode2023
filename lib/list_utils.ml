70e10295e2410da2a6f9e21c30dcfeb2d071cdb5
Thibault Martin <twenty2bar@gmail.com>
2023-12-05 16:15:35 +0100

    Add list_utils module

---
+++ lib/list_utils.ml
@@ -0,0 +1,18 @@
+let iter_pairs ~f l =
+  let rec aux = function
+    | [] -> ()
+    | [ _ ] -> assert false
+    | x1 :: x2 :: tl ->
+      f x1 x2;
+      aux tl
+  in
+  aux l
+
+let map_pairs ~f l =
+  let rec aux acc = function
+    | [] -> acc
+    | [ _ ] -> assert false
+    | x1 :: x2 :: tl ->
+      aux (f x1 x2 :: acc) tl
+  in
+  aux [] l
